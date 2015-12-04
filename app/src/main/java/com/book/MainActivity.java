22289c37adb955a001cdc0bada40bbed46861364
Hananideen <hananee92@gmail.com>
2015-12-04 19:43:39 +0800

    Setup web view

--- app/src/main/java/com/book/MainActivity.java
+++ app/src/main/java/com/book/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentManager;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -15,8 +16,18 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
+import android.webkit.WebView;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
@@ -134,7 +145,52 @@ public class MainActivity extends AppCompatActivity
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            WebView view = (WebView) rootView.findViewById(R.id.webView);
+            view.getSettings().setJavaScriptEnabled(true);
+
+            try {
+                JSONObject obj = new JSONObject(loadJSONFromAsset());
+                JSONArray m_jArry = obj.getJSONArray("toc");
+                ArrayList<HashMap<String, String>> formList = new ArrayList<HashMap<String, String>>();
+                HashMap<String, String> list;
+
+                for (int i = 0; i < m_jArry.length(); i++) {
+                    JSONObject jsonObject = m_jArry.getJSONObject(i);
+                    String chapter_value = jsonObject.getString("title");
+                    String url_value = jsonObject.getString("url");
+
+                    list = new HashMap<String, String>();
+                    list.put("title", chapter_value);
+                    list.put("url", url_value);
+
+                    Log.d("Details-->", jsonObject.getString("url"));
+
+                    formList.add(list);
+
+                    view.loadUrl("file:///android_asset/" + url_value);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
             return rootView;
+        }
+
+        public String loadJSONFromAsset() {
+            String json = null;
+            try {
+                InputStream is = getActivity().getAssets().open("book.json");
+                int size = is.available();
+                byte[] buffer = new byte[size];
+                is.read(buffer);
+                is.close();
+                json = new String(buffer, "UTF-8");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                return null;
+            }
+            return json;
         }
 
         @Override
